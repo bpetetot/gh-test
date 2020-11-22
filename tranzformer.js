775a2dbbefb080794304b7c9d497c3656423843d
Benjamin Petetot <bpetetot@gmail.com>
2020-11-22 17:52:37 +0100

    :sparkles: tranzform sentence

--- tranzformer.js
+++ tranzformer.js
@@ -2,10 +2,14 @@ function tranzformWord (str) {
   return 'Z' + str.slice(1)
 }
 
+function tranzformSentence (str) {
+  return str.split(' ').map(tranzformWord).join(' ')
+}
+
 function tranzform (str) {
   if (!str) return
 
-  const rezult = tranzformWord(str)
+  const rezult = tranzformSentence(str)
 
   return rezult
 }
