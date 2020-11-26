c0c65e1e41c6cdea97233191bac34c49fceffa36
Benjamin Petetot <bpetetot@gmail.com>
2020-11-26 12:07:35 +0100

    Revert ":sparkles: tranzform sentence"
    This reverts commit 3e71bb8493ee52119133e9186bcd264aba479c2b.

--- tranzformer.js
+++ tranzformer.js
@@ -2,14 +2,12 @@ function tranzformWord (str) {
   return 'Z' + str.slice(1)
 }
 
-function tranzformSentence (str) {
-  return str.split(' ').map(tranzformWord).join(' ')
-}
-
 function tranzform (str) {
   if (!str) return
 
-  return tranzformSentence(str)
+  const rezult = tranzformWord(str)
+
+  return rezult
 }
 
 module.exports = { tranzform }
