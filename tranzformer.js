cb81938190067432e724742be5bb20e933d5e8e8
Benjamin Petetot <bpetetot@gmail.com>
2021-04-22 09:41:26 +0200

    Revert ":sparkles: transform sentence"
    This reverts commit 1807ae23bd1bfea3ff3ba5851db69f00cf294e25.

--- tranzformer.js
+++ tranzformer.js
@@ -2,14 +2,10 @@ function tranzformWord (str) {
   return 'Z' + str.slice(1)
 }
 
-function tranzformSentence (str) {
-  return str.split(' ').map(tranzformWord).join(' ')
-}
-
-function tranzform (str) {
+function tranzform(str) {
   if (!str) return
 
-  const rezult = tranzformSentence(str)
+  const rezult = tranzformWord(str)
 
   return rezult
 }
